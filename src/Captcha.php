c6230dbf653f20c7092163c6e2c89af79128cd4d
Ravi Kumar <idea.begins.here@gmail.com>
2015-09-22 22:21:06 +0530

    Initial Commit

---
+++ src/Captcha.php
@@ -0,0 +1,18 @@
+<?php namespace RaviKumar8\Captcha;
+
+/**
+* Captcha
+*/
+class Captcha
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public function render()	{
+
+		
+	}
+}
---
+++ tests/CaptchaTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+use RaviKumar8\Captcha;
+
+/**
+* Captcha
+*/
+class CaptchaTest extends PHPUnit_Framework_TestCase {
+	
+	function __construct()	{
+		# code...
+	}
+
+
+	public function testCaptchaRender()	{
+
+		
+	}
+}
